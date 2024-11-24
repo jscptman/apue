8c538a06f1beda0d2bcfae6f585ee43330ac8cff
jscptman <jscptman@gmail.com>
2024-11-24 23:00:58 +0800

    warning: r_date's dependency iana_time_zone has an bug that get_timezone() can't get the right Tz. See [here](https://github.com/strawlab/iana-time-zone/issues/118) for more details.

--- src/bin/r_date.rs
+++ src/bin/r_date.rs
@@ -1,6 +1,10 @@
-use chrono::Local;
-
+use chrono::{Local, TimeZone};
+use chrono_tz::Tz;
 fn main() {
-    let local = Local::now();
-    println!("{}", local.format("%a %b %e %r %Z %Y").to_string());
+    let tz = iana_time_zone::get_timezone().unwrap();
+    println!("🚀 {:?}", tz); // 🚀 "Etc/UTC"
+    let tz = tz.parse::<Tz>().unwrap();
+    // let tz = Tz::Asia__Shanghai; // my real TZ
+    let dt = tz.from_local_datetime(&Local::now().naive_local()).unwrap();
+    println!("{}", dt.format("%a %b %e %r %Z %Y").to_string()); // Sun Nov 24 10:27:33 PM UTC 2024
 }
