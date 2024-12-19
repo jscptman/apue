d259e1543ba1393a33eef06aeefc29ebc4141fec
jscptman <jscptman@gmail.com>
2024-12-19 11:10:02 +0800

    feat: close_exec.rs

---
+++ src/bin/close_exec.rs
@@ -0,0 +1,46 @@
+use std::{fs::OpenOptions, io, os::fd::AsRawFd};
+
+use nix::{
+    dir::Dir,
+    fcntl::{self, FcntlArg, FdFlag, OFlag},
+    sys::stat::Mode,
+};
+
+fn main() -> Result<(), io::Error> {
+    let dir = OpenOptions::new()
+        .read(true)
+        .open("/")
+        .expect("path open failed");
+    let mut close_exec_flag =
+        fcntl::fcntl(dir.as_raw_fd(), FcntlArg::F_GETFD).expect("fcntl failed");
+    println!(
+        "🚀 rust std open close_exec_flag: {}, global FD_CLOSE_EXEC value is: {:?}",
+        close_exec_flag,
+        FdFlag::FD_CLOEXEC.bits()
+    );
+
+    let dir = Dir::open("/", OFlag::O_RDONLY, Mode::empty())?;
+    close_exec_flag = fcntl::fcntl(dir.as_raw_fd(), FcntlArg::F_GETFD).expect("fcntl failed");
+    println!(
+        "🚀 nix Dir::open close_exec_flag: {}, global FD_CLOSE_EXEC value is: {:?}",
+        close_exec_flag,
+        FdFlag::FD_CLOEXEC.bits()
+    );
+
+    let dir = fcntl::open("/", OFlag::O_RDONLY, Mode::empty())?;
+    close_exec_flag = fcntl::fcntl(dir, FcntlArg::F_GETFD).expect("fcntl failed");
+    println!(
+        "🚀 nix fcntl::open close_exec_flag: {}, global FD_CLOSE_EXEC value is: {:?}",
+        close_exec_flag,
+        FdFlag::FD_CLOEXEC.bits()
+    );
+
+    fcntl::fcntl(dir, FcntlArg::F_SETFD(FdFlag::all()))?;
+    close_exec_flag = fcntl::fcntl(dir, FcntlArg::F_GETFD).expect("fcntl failed");
+    println!(
+        "🚀 after fcntl::setfd close_exec_flag: {}, global FD_CLOSE_EXEC value is: {:?}",
+        close_exec_flag,
+        FdFlag::FD_CLOEXEC.bits()
+    );
+    Ok(())
+}
