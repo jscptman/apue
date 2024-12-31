5fa85a43d24935055291071e0f79b54356187381
jscptman <jscptman@gmail.com>
2024-12-31 14:56:19 +0800

    feat：fmemopen(in rust is &mut [u8] or Cursor)

--- crates/chapter_5/src/bin/5.7.rs
+++ crates/chapter_5/src/bin/5.7.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use std::io::{self, Cursor, Read, Write};
+
+fn main() -> io::Result<()> {
+    let buffer = [0; 100];
+    let mut cursor = Cursor::new(buffer);
+    write!(&mut cursor, "Hello, world!")?;
+    cursor.set_position(0);
+    let mut read_buffer = [0; 100];
+    let _n = cursor.read(&mut read_buffer)?;
+    println!(
+        "Read from memory: {:?}",
+        String::from_utf8_lossy(
+            &read_buffer
+                .into_iter()
+                .filter(|c| { *c > 0 })
+                .collect::<Vec<u8>>()
+        )
+    );
+    Ok(())
+}
